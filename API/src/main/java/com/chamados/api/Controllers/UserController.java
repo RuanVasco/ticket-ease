ea4aa8fa78ee9d99293978286f1a922ba610bde8
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-07-02 11:54:36 -0300

    created table template

---
+++ API/src/main/java/com/chamados/api/Controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.chamados.api.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chamados.api.Repositories.UserRepository;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(userRepository.findAllWithoutPassword());
+    }
+}
