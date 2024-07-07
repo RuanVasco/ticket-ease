a0bb4ec4dfa660a0b74f23153f1bbd39b73f52b2
Laura Hauschild <ruanvasconcelos898@gmail.com>
2024-07-07 18:54:25 -0300

    cors disabled

--- API/src/main/java/com/chamados/api/SecurityConfig.java
+++ API/src/main/java/com/chamados/api/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,9 +27,9 @@ public class SecurityConfig {
 	@Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
-				.csrf(csrf-> csrf.disable())
+				.csrf(AbstractHttpConfigurer::disable)
 				.headers(headers -> headers
-						.frameOptions(frameOptions -> frameOptions.sameOrigin()) 
+						.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
 	            )
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 						.authorizeHttpRequests(authorize -> authorize
