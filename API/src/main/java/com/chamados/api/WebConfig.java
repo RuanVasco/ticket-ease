66ef08a593925769b90de7a22e5ca0a5fdb0bb31
Laura Hauschild <ruanvasconcelos898@gmail.com>
2024-07-07 19:28:26 -0300

    allow all hosts

--- API/src/main/java/com/chamados/api/WebConfig.java
+++ API/src/main/java/com/chamados/api/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // Adjust to your frontend's URL
+                .allowedOrigins("*")  // Adjust to your frontend's URL
                 .allowedMethods("*");  // Allow all methods
     }
 }
