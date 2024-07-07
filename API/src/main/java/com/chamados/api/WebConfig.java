d5efe0ee416d0c81ba7920fc57ff576b540aba51
Laura Hauschild <ruanvasconcelos898@gmail.com>
2024-07-07 19:16:50 -0300

    aaaaaaaaa

--- API/src/main/java/com/chamados/api/WebConfig.java
+++ API/src/main/java/com/chamados/api/WebConfig.java
@@ -6,8 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")  // Adjust to your frontend's URL
+                .allowedMethods("*");  // Allow all methods
     }
 }
