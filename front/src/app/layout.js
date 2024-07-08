1a2fd61c304a998a9576f637eb7743d937a20b9d
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-07-08 11:25:46 -0300

    changed forms font color

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -1,17 +1,17 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ImportBsJS from "./components/importBsJS";
 import "./globals.css";
 
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) { 
 
   return (
     <html lang="pt-br">
       <ImportBsJS />
-      <body className={inter.className}>
+      <body>
         {children}
       </body>
     </html>
