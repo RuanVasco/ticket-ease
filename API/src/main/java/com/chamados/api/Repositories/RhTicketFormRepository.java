c860586dfac02ed68ce1c73651e33d949eaf021b
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-07-23 07:42:03 -0300

    removed unused files

--- API/src/main/java/com/chamados/api/Repositories/RhTicketFormRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chamados.api.Repositories;
-
-import com.chamados.api.Tickets.RhTicketForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RhTicketFormRepository extends JpaRepository<RhTicketForm, Long> {
-}
--- API/src/main/java/com/chamados/api/Repositories/TiTicketFormRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chamados.api.Repositories;
-
-import com.chamados.api.Tickets.TiTicketForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TiTicketFormRepository extends JpaRepository<TiTicketForm, Long> {
-}
