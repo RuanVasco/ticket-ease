24eda20de077ad7073a7a2bc3e5270d33cde55e3
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-07-31 10:31:02 -0300

    open ticket screen is ok now

--- front/src/app/chamados/abrir/page.js
+++ front/src/app/chamados/abrir/page.js
@@ -3,7 +3,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../../components/header/header';
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
+import AttachmentsForm from '../../components/attachmentsForm/attachmentsForm';
+import { FaAngleRight, FaAngleLeft, FaFolderOpen, FaClipboardList } from "react-icons/fa6";
 import style from "./style.css";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
@@ -130,86 +131,137 @@ const AbrirChamado = () => {
     };
 
     const handleCategoryClick = (category) => {
-        console.log(categoryPath);
         setCurrentCategory(category);
         setCategoryPath([...categoryPath, category]);
     };
 
     const handleBack = () => {
         const newPath = [...categoryPath];
-        
+
         newPath.pop();
-        setCurrentCategory(newPath.length > 0 ? newPath[newPath.length - 1] : organizeData());        
+        setCurrentCategory(newPath.length > 0 ? newPath[newPath.length - 1] : organizeData());
         setCategoryPath(newPath);
     };
 
     const renderTreeNavigation = (data) => {
-        return data.children.map((item) => {
-            return (
-                <li key={item.id} className="list-tree-item">
-                    {categoryPath.length > 0 && (
-                        <div>
-                            <button onClick={handleBack} className="btn btn-link">
-                                <FaAngleLeft className="me-2" /> Voltar
-                            </button>
-                            {categoryPath.map((category, index) => (
-                                <span key={index}>{category.name} / </span>
-                            ))}
-                        </div>
-                    )}
-
-                    {item.children ? (
-                        <div>
-                            <a
-                                onClick={() => handleCategoryClick(item)}
-                                className="toggle-tree-nav"
-                                role="button"
-                                style={{ cursor: 'pointer' }}
-                            >
-                                {item.name}
-                                <FaAngleRight className="ms-3" />
-                            </a>
-                            {currentCategory === item && (
-                                <ul className="list-tree ps-4">
-                                    {renderTreeNavigation(item)}
-                                </ul>
-                            )}
-                        </div>
-                    ) : (
-                        <a
-                            style={{ cursor: 'pointer' }}
-                            value={item.id}
-                            onClick={() => setCurrentForm(item)}
+        return (
+            <div>
+                <div className='nav_forms_cat_head'>
+                    {categoryPath.length > 0 ? (
+                        <button
+                            onClick={handleBack}
+                            className="nav_forms_cat_btn_back"
                         >
-                            {item.name} (é formulário)
-                        </a>
+                            <FaAngleLeft />
+                            <span className="ms-2">{categoryPath.length > 0 && categoryPath[categoryPath.length - 1].name}</span>
+                        </button>
+                    ) : (
+                        <span>/</span>
                     )}
-                </li>
-            );
-        });
+                </div>
+
+                <ul className="mt-2 nav_forms_cat_list">
+                    {data.children
+                        .filter(item => item.children && item.children.length > 0 || !item.children)
+                        .map((item) => (
+                            <li key={item.id} className={`nav_forms_cat_item${currentForm === item ? ' item_selected' : ''}`}>
+                                {item.children && item.children.length > 0 ? (
+                                    <div>
+                                        <a
+                                            onClick={() => handleCategoryClick(item)}
+                                            className="d-flex align-items-center"
+                                            role="button"
+                                            style={{ cursor: 'pointer' }}
+                                        >
+                                            <FaFolderOpen />
+                                            <span className="ms-2">{item.name}</span>
+                                            <FaAngleRight className='ms-auto' />
+                                        </a>
+                                        {currentCategory === item && (
+                                            <div>
+                                                {renderTreeNavigation(item)}
+                                            </div>
+                                        )}
+                                    </div>
+                                ) : !item.children ? (
+                                    <a
+                                        style={{ cursor: 'pointer' }}
+                                        onClick={() => setCurrentForm(item)}
+                                        className="d-flex align-items-center"
+                                    >
+                                        <FaClipboardList />
+                                        <span className="ms-2">{item.name}</span>
+                                    </a>
+                                ) : null}
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        );
     };
 
     return (
         <main>
             <Header pageName="Abrir Chamado" />
             <div className="container">
-                <div className="row">
-                    <div className="col-2 p-0 border-end">
-                        <ul className="list-tree">
-                            {currentCategory ? renderTreeNavigation(currentCategory) : null}
-                        </ul>
+                <div className="row mt-3">
+                    <div className="col-2">
+                        {currentCategory ? renderTreeNavigation(currentCategory) : null}
                     </div>
-                    <div className="col">
-                        {form && (
+                    <div className="col border-start ps-4">
+                        {form ? (
                             <div>
-                                <h2>{form.name}</h2>
+                                <h4 className="fw-semibold border-bottom pb-1 ps-2">{currentCategory.name + " / " + form.name}</h4>
                                 <form>
-                                    <input type="text" placeholder="Assunto" className="form-control mb-3" />
-                                    <textarea className="form-control mb-3" placeholder="Descrição"></textarea>
-                                    <button type="submit" className="btn btn-primary">Abrir Chamado</button>
+                                    <input className="d-none" readOnly value={form.id}></input>
+                                    <div className="row">
+                                        <div className="col">
+                                            <label htmlFor="ticket-name" className="form-label mb-1">Assunto: </label>
+                                            <input type="text" name="ticket-name" className="form-control" required />
+                                        </div>
+                                    </div>
+                                    <div className="row mt-2">
+                                        <div className="col">
+                                            <label htmlFor="ticket-description" className="form-label mb-1">Descrição: </label>
+                                            <textarea className="form-control" name="ticket-description" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div className="row mt-2">
+                                        <div className="col">
+                                            <label htmlFor="ticket-observation" className="form-label mb-1">Observação: </label>
+                                            <textarea className="form-control" name="ticket-observation"></textarea>
+                                        </div>
+                                    </div>
+                                    <div className="row mt-2">
+                                        <div className="col">
+                                            <label htmlFor="ticket-attachments" className="form-label">Anexos: </label>
+                                            <AttachmentsForm />
+                                        </div>
+                                    </div>
+                                    <div className="row mt-2">
+                                        <div className="col">
+                                            <label htmlFor="ticket-urgency" className="form-label">Urgência: </label>
+                                            <select className="ms-3" name="ticket-urgency" required>
+                                                <option>Alta</option>
+                                                <option selected>Média</option>
+                                                <option>Baixa</option>
+                                            </select>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="ticket-receiveEmail" name="ticket-receiveEmail" />
+                                                <label class="form-check-label" htmlFor="ticket-receiveEmail">
+                                                    Receber Atualizações por E-mail
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>                                    
+                                    <button type="submit" className="btn btn-primary mt-3">Abrir Chamado</button>
                                 </form>
                             </div>
+                        ) : (
+                            <p>Selecione uma categoria ou formulário para abrir um chamado.</p>
                         )}
+                    </div>
+                    <div className="col-2">
                     </div>
                 </div>
             </div>
--- front/src/app/components/attachmentsForm/attachmentsForm.js
+++ front/src/app/components/attachmentsForm/attachmentsForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaCircleXmark } from "react-icons/fa6";
 
 const AttachmentsForm = () => {
     const [fileInputs, setFileInputs] = useState([0]);
@@ -10,18 +11,41 @@ const AttachmentsForm = () => {
         }
     };
 
+    const cleanInput = (index) => {
+        const inputElement = document.querySelector(`input[name='attachments${index}']`);
+
+        if (inputElement) {
+            if (fileInputs.length === 1 && inputElement.files.length > 0) {
+                inputElement.value = ''; 
+            } else {
+                if (fileInputs.length > 1) {
+                    setFileInputs(fileInputs.filter((_, i) => i !== index));
+                }
+            }
+        }
+    };
+
     return (
         <div id="attachmentsForm" className="attachmentsForm">
             {fileInputs.map((input, index) => (
                 <div key={index}>
-                    <input 
-                        type="file" 
-                        name={`attachments${index}`} 
-                        className="attachments mt-2 form-control" 
-                        multiple 
-                        accept="image/*"
-                        onChange={handleFileChange(index)}
-                    />
+                    <div className="input-group mb-2">
+                        <input
+                            type="file"
+                            name={`attachments${index}`}
+                            className="attachments form-control"
+                            multiple
+                            accept="image/*"
+                            onChange={handleFileChange(index)}
+                        />
+                        <button
+                            className="btn btn-outline-secondary"
+                            type="button"
+                            onClick={() => cleanInput(index)}
+                        >
+                            <FaCircleXmark />
+                        </button>
+                    </div>
                 </div>
             ))}
         </div>
