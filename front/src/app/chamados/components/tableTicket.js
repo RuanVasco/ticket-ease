90ded4628b43fafb04b734931e54786f43374b20
Mateus Schneider <mateus2112schneider@gmail.com>
2024-08-02 19:51:53 -0300

    rodapé da pagina

--- front/src/app/chamados/components/tableTicket.js
+++ front/src/app/chamados/components/tableTicket.js
@@ -9,6 +9,7 @@ const TableTicket = ({ viewMode = 'readonly' }) => {
     const [currentPage, setCurrentPage] = useState(0);
     const [pageSize, setPageSize] = useState(10);
     const [totalPages, setTotalPages] = useState(0);
+    const pages = [...Array(totalPages).keys()];
 
     const columns = [
         { label: 'Formulário', value: 'form.name' },
@@ -131,7 +132,40 @@ const TableTicket = ({ viewMode = 'readonly' }) => {
                     Próximo
                 </button>
             </div>
+            <div className="d-flex flex-column align-items-center justify-content-end">
+            <ul className="pagination">
+                <li className="page-item">
+                    <button
+                        className="page-link"
+                        onClick={() => onPageChange(Math.max(0, currentPage - 1))}
+                        disabled={currentPage === 0}
+                    >
+                        Anterior
+                    </button>
+                </li>
+                {pages.map(page => (
+                    <li key={page} className={`page-item ${page === currentPage ? 'active-page' : ''}`}>
+                        <button
+                            className="page-link"
+                            onClick={() => {setCurrentPage(page)}}
+                        >
+                            {page + 1} 
+                        </button>
+                    </li>
+                ))}
+                <li className="page-item">
+                    <button
+                        className="page-link"
+                        onClick={() => onPageChange(Math.min(totalPages - 1, currentPage + 1))}
+                        disabled={currentPage === totalPages - 1}
+                    >
+                        Próximo
+                    </button>
+                </li>
+            </ul>            
         </div>
+        </div>
+        
     );
 };
 
